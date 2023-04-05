72f042ca356a659f0c37e285515d49f691010593
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2023-04-05 12:40:29 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require("express")
+const cors = require('cors')
+const dotenv = require('dotenv').config()
+console.log()
+// app config
+const app=express() 
+app.use(cors())
+app.use(express.json())
+const port=process.env.PORT || 3001
+
+
+app.get('/',(req,res)=>{
+        res.send("we're live !!")
+})
+app.post('/pay',async(req,ress)=>{
+  if(req.headers.authorization == process.env.AUTHORIZATION_KEY){
+    const{email}=req.body
+    const https = require('https')
+    const params = JSON.stringify({
+      "email": email,
+      "amount": "10000"
+    })  
+  
+  const options = {
+    hostname: 'api.paystack.co',
+    port: 443,
+    path: '/transaction/initialize',
+    method: 'POST',
+    headers: {
+      Authorization: process.env.SECRET_KEY,
+      'Content-Type': 'application/json'
+    }
+  }
+  
+  let data = ''
+  const pay = https.request(options, res => {
+  
+    res.on('data', (chunk) => {
+      data += chunk
+      ress.send(data)
+  
+    });
+    res.on('end', () => {   
+      console.log(JSON.parse(data))
+    })
+  }).on('error', error => {
+    console.error(error)
+    ress.send(error)
+  
+  })
+  
+  pay.write(params)
+  pay.end()
+}else{
+  ress.json('unauthorized user')
+}
+  
+  })   
+  app.post('/verify',async(req,ress)=>{
+    const https = require('https')
+    const {reference}=req.body  
+    console.log(reference)
+    const options = {
+      hostname: 'api.paystack.co',
+      port: 443,
+      path: '/transaction/verify/'+ reference,
+      method: 'GET',
+      headers: {  
+        Authorization: 'Bearer sk_live_ec749266c4dead70ed0a053fd3b50e5378e19d58'
+      }
+    }
+    
+    https.request(options, res => {
+      let data = ''  
+    
+      res.on('data', (chunk) => {
+        data += chunk
+      });
+  
+      res.on('end', () => {
+        console.log(JSON.parse(data))
+        ress.send(data)
+      })
+    }).on('error', error => {
+      console.error(error)
+    })
+  
+  })
+
+
+app.listen(port,()=>console.log('listening an port '+ port))
